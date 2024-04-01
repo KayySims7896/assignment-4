c87363f8b192726353a6186f3e6e65c3cc39d55b
hchen114 <161513508+hchen114@users.noreply.github.com>
2024-04-01 13:37:43 -0400

    grow flower code, hold to grow
    2 errors, not sure where they are, used code from online & tried to match to what i had

---
+++ Assets/scripts/growflower.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//code from: click and hold to grow https://www.youtube.com/watch?v=SyRCSiQdpaw
+
+public class growflower : MonoBehaviour
+{
+    Vector3 scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+
+    private GameObject selectedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //click and hold down on object to grow
+        if (Input.clickCount > 0 && Input.touches[0].phase == TouchPhase.Stationary)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray.out hit))
+            {
+                if (hit.transform.tag == "sunflower")
+                {
+                    selectedObject = hit.collider.gameObject;
+
+                    selectedObject.transform.localScale += scaleChange;
+
+                    if (hit.transform.localScale.y < 1f || hit.transform.localScale.y > 5f)
+                    {
+                        scaleChange = selectedObject;
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
