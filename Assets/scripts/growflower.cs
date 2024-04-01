e08c20c261d2d8d629ead0267f5bb8af766b34e8
Reiley Folch <reileyfolch@Reileys-MacBook-Pro.local>
2024-04-01 19:02:32 -0400

    Update growflower.cs
    Final Edit

--- Assets/scripts/growflower.cs
+++ Assets/scripts/growflower.cs
@@ -2,25 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//code from: click and hold to grow https://www.youtube.com/watch?v=SyRCSiQdpaw
-
-public class growflower : MonoBehaviour
+public class GrowFlower : MonoBehaviour
 {
-    Vector3 scaleChange = new Vector3(-0.1f, -0.1f, -0.1f);
-
+    private Vector3 scaleChange = new Vector3(0.1f, 0.1f, 0.1f);
     private GameObject selectedObject;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        //click and hold down on object to grow
-        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Stationary)
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Moved)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
 
@@ -28,22 +18,39 @@ public class growflower : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.transform.tag == "sunflower")
+                if (hit.transform.CompareTag("sunflower"))
                 {
                     selectedObject = hit.collider.gameObject;
 
                     selectedObject.transform.localScale += scaleChange;
 
-                    if (hit.transform.localScale.y < 1f || hit.transform.localScale.y > 5f)
+                    if (selectedObject.transform.localScale.y < 1f || selectedObject.transform.localScale.y > 5f)
                     {
-                        scaleChange = new Vector3(-0.1f, -0.1f, -0.1f);
+                        scaleChange *= -1;
                     }
-                    
                 }
             }
         }
+        else if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            RaycastHit hit;
 
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("sunflower"))
+                {
+                    selectedObject = hit.collider.gameObject;
 
+                    selectedObject.transform.localScale += scaleChange;
+
+                    if (selectedObject.transform.localScale.y < 1f || selectedObject.transform.localScale.y > 5f)
+                    {
+                        scaleChange *= -1;
+                    }
+                }
+            }
+        }
     }
 }
